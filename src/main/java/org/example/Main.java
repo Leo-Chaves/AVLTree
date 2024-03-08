beb524cb938a7b1da8ba45b5b8b789bbf517c901
Leo-Chaves <leonardo.2021107241@unicap.br>
2024-03-08 02:22:17 -0300

    Avl

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,17 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        Tree avl = new Tree();
+        avl.insert(1);
+        avl.insert(2);
+        avl.insert(3);
+
+        System.out.println();
+        System.out.println(avl.getRoot().getInfo());
+        System.out.println();
+        System.out.println(avl.getRoot().getLeft().getInfo());
+        System.out.println();
+        System.out.println(avl.getRoot().getLeft().getLeft().getInfo());
 
     }
 }
--- src/main/java/org/example/Tree.java
+++ src/main/java/org/example/Tree.java
@@ -1,8 +1,15 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Tree<T extends Comparable <T>> {
     private Node<T> root;
     private boolean status;
+
+    public Tree() {
+
+    }
 
     private boolean isEmpty() {
         return root == null;
@@ -52,18 +59,77 @@ public class Tree<T extends Comparable <T>> {
     }
 
     private Node<T> rotateLeft(Node<T> r) {
-        Node<T> a;
-        Node<T> b;
-        Node<T> c;
+        Node<T> a = r.getRight();
+        Node<T> b = a.getLeft();
+        Node<T> c = r;
 
-        return r;
+        if (a.getBalance() == -1) {
+            // Caso de rotação dupla à direita
+            Node<T> d = b.getRight();
+            Node<T> e = b.getLeft();
+
+            // Rotação à esquerda na subárvore Y
+            b.setLeft(c);
+            c.setRight(d);
+
+            // Rotação à direita no nó X
+            r.setRight(b);
+            b.setLeft(a);
+
+            // Atualização dos fatores de balanceamento
+            c.setBalance(0);
+            b.setBalance(0);
+            a.setBalance(1);
+        } else {
+            // Caso de rotação simples à esquerda
+            r.setRight(b);
+            a.setLeft(c);
+
+            // Atualização dos fatores de balanceamento
+            c.setBalance(0);
+            a.setBalance(0);
+        }
+
+        return a;
     }
 
     private Node<T> rotateRight(Node<T> r) {
-        Node<T> a;
-        Node<T> b;
-        Node<T> c;
+        Node<T> a = r.getLeft();
+        Node<T> b = a.getRight();
+        Node<T> c = r;
 
-        return r;
+        if (a.getBalance() == 1) {
+            // Caso de rotação dupla à esquerda
+            Node<T> d = b.getLeft();
+            Node<T> e = b.getRight();
+
+            // Rotação à direita na subárvore Y
+            b.setRight(c);
+            c.setLeft(d);
+
+            // Rotação à esquerda no nó X
+            r.setLeft(b);
+            b.setRight(a);
+
+            // Atualização dos fatores de balanceamento
+            c.setBalance(0);
+            b.setBalance(0);
+            a.setBalance(-1);
+        } else {
+            // Caso de rotação simples à direita
+            r.setLeft(b);
+            a.setRight(c);
+
+            // Atualização dos fatores de balanceamento
+            c.setBalance(0);
+            a.setBalance(0);
+        }
+
+        return a;
+    }
+
+
+    public Node<T> getRoot() {
+        return root;
     }
 }
